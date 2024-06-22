4ab2f3fceb45e644e4c41d99731e4dc56e7f43b6
yymin1022 <yymin1022@gmail.com>
2024-06-23 02:40:41 +0900

    Added meter_page.dart

---
+++ lib/pages/meter_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MeterPage extends StatelessWidget {
+  const MeterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text("Meter Page")
+          ],
+        ),
+      )
+    );
+  }
+}
