edbccd081b39b6bec9f8425cb7450408a8faa6b9
yymin1022 <yymin1022@gmail.com>
2024-06-23 02:46:41 +0900

    Update main.dart : Cleaned UP

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TaxiMeterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TaxiMeterApp extends StatelessWidget {
+  const TaxiMeterApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Taxi Meter',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TaxiMeterMain(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class TaxiMeterMain extends StatefulWidget {
+  const TaxiMeterMain({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TaxiMeterMain> createState() => _TaxiMeterMainState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _TaxiMeterMainState extends State<TaxiMeterMain> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Text("Taxi Meter"),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
