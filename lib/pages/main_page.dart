ee7a5978904870f517812867dbe0868a40747d3e
yymin1022 <yymin1022@gmail.com>
2024-06-24 03:29:22 +0900

    Update main_page.dart : Added Navigator Call for Inkwell

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,48 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:taximeter/pages/meter_page.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.local_taxi,
-              size: 224.0
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "택시",
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 48.0,
-                    fontWeight: FontWeight.bold
+    return Scaffold(
+      body: InkWell(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(
+            builder: (context) {
+              return MeterPage();
+            }
+          ));
+        },
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Icon(
+                Icons.local_taxi,
+                size: 224.0
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "택시",
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 48.0,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  Text(
+                    "미터기",
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 48.0
+                    )
                   )
-                ),
-                Text(
-                  "미터기",
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 48.0
-                  )
+                ],
+              ),
+              Text(
+                "터치하여 시작하기",
+                style: TextStyle(
+                  color: Colors.black45,
+                  fontSize: 24.0
                 )
-              ],
-            ),
-            Text(
-              "터치하여 시작하기",
-              style: TextStyle(
-                color: Colors.black45,
-                fontSize: 24.0
               )
-            )
-          ],
-        ),
+            ],
+          ),
+        )
       )
     );
   }
