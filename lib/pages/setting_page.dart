6e1880e879b72f95021373a98d11804ee82ed594
yymin1022 <yymin1022@gmail.com>
2024-06-26 03:32:25 +0900

    Update settings_items.xml : Added Setting List Elements

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -10,15 +10,52 @@ class SettingPage extends StatelessWidget {
         padding: const EdgeInsets.only(left: 10.0, right: 10.0),
         child: ListView(
           children: [
-            const SettingListTitle("asdf"),
+            const SettingListTitle("미터기 설정"),
             ListTile(
-              title: Text("Preference Title"),
-              subtitle: Text("Preference Value"),
+              title: Text("위치"),
+              subtitle: Text("서울특별시"),
               onTap: (){},
             ),
             ListTile(
-              title: Text("Preference Title"),
-              subtitle: Text("Preference Value"),
+              title: Text("미터기 테마"),
+              subtitle: Text("말 타입"),
+              onTap: (){},
+            ),
+            const SettingListTitle("미터기 정보"),
+            ListTile(
+              title: Text("요금 정보"),
+              subtitle: Text("기본요금 : 4800원 (최초 1.6km)"),
+              onTap: (){},
+            ),
+            ListTile(
+              title: Text("요금정보 DB 버전"),
+              subtitle: Text("20240310"),
+              onTap: (){},
+            ),
+            const SettingListTitle("개발자 정보"),
+            ListTile(
+              title: Text("Dev. LR"),
+              subtitle: Text("중앙대학교 소프트웨어학부 2019"),
+              onTap: (){},
+            ),
+            ListTile(
+              title: Text("개발자 블로그"),
+              subtitle: Text("https://dev-lr.com"),
+              onTap: (){},
+            ),
+            ListTile(
+              title: Text("개발자 GitHub"),
+              subtitle: Text("https://github.com/yymin1022"),
+              onTap: (){},
+            ),
+            ListTile(
+              title: Text("개발자 Instagram"),
+              subtitle: Text("https://instagram.com/useful_min"),
+              onTap: (){},
+            ),
+            ListTile(
+              title: Text("개인정보 처리방침"),
+              subtitle: Text("https://defcon.or.kr/privacy"),
               onTap: (){},
             )
           ],
