171ff11b7b50a44b96e4ee06be26175c24f3e8db
yymin1022 <yymin1022@gmail.com>
2024-06-26 03:18:11 +0900

    Added preference_util.dart

---
+++ lib/utils/preference_util.dart
@@ -0,0 +1,10 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferenceUtil {
+  PreferenceUtil._privateConstructor();
+  static final PreferenceUtil _instance = PreferenceUtil._privateConstructor();
+
+  factory PreferenceUtil() {
+    return _instance;
+  }
+}
