1ee3a9704b63e8011134eabdb95ec4493297d81c
yymin1022 <yymin1022@gmail.com>
2024-06-26 03:13:38 +0900

    Update settings_items.xml : Added Setting List Elements

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -5,13 +5,42 @@ class SettingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+        child: ListView(
           children: [
-            Text("Setting Page")
+            const SettingListTitle("asdf"),
+            ListTile(
+              title: Text("Preference Title"),
+              subtitle: Text("Preference Value"),
+              onTap: (){},
+            ),
+            ListTile(
+              title: Text("Preference Title"),
+              subtitle: Text("Preference Value"),
+              onTap: (){},
+            )
           ],
         ),
+      )
+    );
+  }
+}
+
+class SettingListTitle extends StatelessWidget {
+  const SettingListTitle(this.text, {super.key});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0, left: 15.0, right: 15.0),
+      child: Text(text,
+        style: const TextStyle(
+          color: Colors.black38,
+          fontSize: 15.0
+        )
       )
     );
   }
