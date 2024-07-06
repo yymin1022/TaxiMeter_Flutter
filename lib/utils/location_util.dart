97b4b74c29c38e23a1120bf1b9089294787f1db7
yymin1022 <yymin1022@gmail.com>
2024-07-07 03:11:45 +0900

    Update location_util : Added Location Datas

--- lib/utils/location_util.dart
+++ lib/utils/location_util.dart
@@ -1,5 +1,21 @@
 enum Location {
-  defaultItem("default_item", en: "Default", ko:  "기본");
+  seoul("seoul", en: "Seoul", ko: "서울특별시"),
+  gangwon("seoul", en: "Gangwon", ko: "강원도"),
+  gyeonggi("seoul", en: "Gyeonggi", ko: "경기도"),
+  gyeongbuk("seoul", en: "Gyeongbuk", ko: "경상북도"),
+  gyeongnam("seoul", en: "Gyeongnam", ko: "경상남도"),
+  gwangju("seoul", en: "Gwangju", ko: "광주광역시"),
+  daegu("seoul", en: "Daegu", ko: "대구광역시"),
+  daejeon("seoul", en: "Daejeon", ko: "대전광역시"),
+  busan("seoul", en: "Busan", ko: "부산광역시"),
+  ulsan("seoul", en: "Ulsan", ko: "울산광역시"),
+  incheon("seoul", en: "Incheon", ko: "인천광역시"),
+  jeonbuk("seoul", en: "Jeonbuk", ko: "전라북도"),
+  jeonnam("seoul", en: "Jeonnam", ko: "전라남도"),
+  jeju("seoul", en: "Jeju", ko: "제주특별자치도"),
+  chungbuk("seoul", en: "Chungbuk", ko: "충청북도"),
+  chungnam("seoul", en: "Chungnam", ko: "충청남도"),
+  custom("seoul", en: "Custom", ko: "직접 설정");
 
   const Location(this.code, {required this.en, required this.ko});
   final String code;
