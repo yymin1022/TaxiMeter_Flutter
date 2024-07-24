f6c86fd1c2c838a2fb09f97a4edfe2ed3d98a029
yymin1022 <yymin1022@gmail.com>
2024-07-24 23:48:06 +0900

    Added firebase_util

---
+++ lib/utils/firebase_util.dart
@@ -0,0 +1,8 @@
+class FirebaseUtil {
+  FirebaseUtil._privateConstructor();
+  static final FirebaseUtil _instance = FirebaseUtil._privateConstructor();
+
+  factory FirebaseUtil() {
+    return _instance;
+  }
+}
