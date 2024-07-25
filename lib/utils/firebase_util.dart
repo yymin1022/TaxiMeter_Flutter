f4b1e4e473510d53c0595f9229698b1ce1c0ba86
yymin1022 <yymin1022@gmail.com>
2024-07-25 11:36:16 +0900

    Update firebase_util.dart : Added Firebase Crashlytics Initialization

--- lib/utils/firebase_util.dart
+++ lib/utils/firebase_util.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
 import 'package:taximeter/firebase_options.dart';
 
 class FirebaseUtil {
@@ -8,6 +12,12 @@ class FirebaseUtil {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
   }
 
   bool isUpdateAvail() {
