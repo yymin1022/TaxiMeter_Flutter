be867a7cc9565cf1e573ff097b6ed58b80e9de2c
yymin1022 <yymin1022@gmail.com>
2024-08-03 18:21:01 +0900

    Update meter_page : Applied MeterUtil Logic

--- lib/pages/meter_page.dart
+++ lib/pages/meter_page.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:taximeter/utils/meter_util.dart';
 
 class MeterPage extends StatelessWidget {
   const MeterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(child:
         Center(
           child: Column(
             children: [
-              Text("Meter Page")
+              Text("Cost : ${MeterUtil().cost}"),
+              Text("Counter : ${MeterUtil().costCounter}"),
+              Text("Cost Mode : ${MeterUtil().costMode}"),
+              Text("Meter Status : ${MeterUtil().meterStatus}"),
+              Text("Current Speed : ${MeterUtil().curSpeed}"),
+              Text("Drived Distance : ${MeterUtil().sumDistance}"),
             ],
           ),
         )
