a6a019517626bd7b2fbd9c96dd95f1cc362cb7d6
yymin1022 <yymin1022@gmail.com>
2024-08-03 17:57:13 +0900

    Update meter_util : Added Enum Classes

--- lib/utils/meter_util.dart
+++ lib/utils/meter_util.dart
@@ -6,3 +6,15 @@ class MeterUtil {
     return _instance;
   }
 }
+
+enum CostMode {
+  COST_BASE,
+  COST_DISTANCE,
+  COST_TIME
+}
+
+enum MeterStatus {
+  METER_NOT_RUNNING,
+  METER_RUNNING,
+  METER_GPS_ERROR
+}
