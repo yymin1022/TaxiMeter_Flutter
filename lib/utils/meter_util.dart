21181af38fd6b6fcd27def2e56f67aa3acb4a7a0
yymin1022 <yymin1022@gmail.com>
2024-08-03 17:52:08 +0900

    Added meter_util

---
+++ lib/utils/meter_util.dart
@@ -0,0 +1,8 @@
+class MeterUtil {
+  MeterUtil._privateConstructor();
+  static final MeterUtil _instance = MeterUtil._privateConstructor();
+
+  factory MeterUtil() {
+    return _instance;
+  }
+}
