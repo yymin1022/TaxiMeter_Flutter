65eb6f53a6e5e5d6ec410bb2b99279a80e6634c4
yymin1022 <yymin1022@gmail.com>
2024-08-09 23:33:44 +0900

    Update color_util : Defined Colors from Android Project

---
+++ lib/utils/color_util.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MeterColor {
+  static const Color meterBackground = Color(0xFF111111);
+  static const Color meterBlue = Color(0xFF87CEEB);
+  static const Color meterGreen = Color(0xFF99EE90);
+  static const Color meterMint = Color(0xFF61D0BB);
+  static const Color meterRed = Color(0xFFDB7093);
+  static const Color meterYellow = Color(0xFFFFED83);
+  static const Color meterBtnText = Color(0xFF444444);
+  static const Color meterTextColorGray = Color(0xFFEEEEEE);
+  static const Color meterTextColorWhite = Color(0xFFCCCCCC);
+}
