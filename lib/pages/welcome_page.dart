d3d28ebb93a84e5f136dd3308596b0ad697ecacf
yymin1022 <yymin1022@gmail.com>
2024-08-19 21:34:57 +0900

    Update welcome_page : Added child pages

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -8,12 +8,67 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  int _welcomeIdx = 0;
+  final List<Widget> _welcomePages = const [
+    WelcomePageInit(),
+    WelcomePagePermission(),
+    WelcomePageLocation()
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Text("Welcome Page"),
+        child: _welcomePages[_welcomeIdx],
       ),
     );
+  }
+}
+
+class WelcomePageInit extends StatefulWidget {
+  const WelcomePageInit({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _WelcomePageInitState();
+}
+
+class _WelcomePageInitState extends State<WelcomePageInit> {
+  //TODO: Welcome Message
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Welcome Page 1. Init");
+  }
+}
+
+class WelcomePageLocation extends StatefulWidget {
+  const WelcomePageLocation({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _WelcomePageLocationState();
+}
+
+class _WelcomePageLocationState extends State<WelcomePageLocation> {
+  //TODO: Warning Text / Location Setup Info
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Welcome Page 3. Location Info");
+  }
+}
+
+class WelcomePagePermission extends StatefulWidget {
+  const WelcomePagePermission({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _WelcomePagePermissionState();
+}
+
+class _WelcomePagePermissionState extends State<WelcomePagePermission> {
+  //TODO: Location Permission
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Welcome Page 2. Permission");
   }
 }
