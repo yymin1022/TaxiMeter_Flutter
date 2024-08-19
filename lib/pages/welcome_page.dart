90e8bc62fe04cd7aae0056560405213b21172ffa
yymin1022 <yymin1022@gmail.com>
2024-08-19 21:03:12 +0900

    Added welcome_page

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Text("Welcome Page"),
+      ),
+    );
+  }
+}
