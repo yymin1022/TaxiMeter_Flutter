b825411c391e13464da423da8a51f942431beb36
yymin1022 <yymin1022@gmail.com>
2024-08-24 16:12:12 +0900

    Update Project : Added CAULY Plugin Registration for Android

--- android/app/src/main/kotlin/com/yong/taximeter/MainActivity.kt
+++ android/app/src/main/kotlin/com/yong/taximeter/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.yong.taximeter
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine.plugins.add(CaulyFlutterSdkPlugin())
+    }
 }
