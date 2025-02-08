da0c0a238a6090d0a1098f2e0931a1eec3325168
yymin1022 <yymin1022@gmail.com>
2025-02-08 20:15:12 +0900

    Update meter_page : Added Landscape Layout Widget

---
+++ lib/pages/meter_page/meter_page_landscape.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MeterPageLandscpe extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MeterPageLandscapeState();
+}
+
+class _MeterPageLandscapeState extends State<MeterPageLandscpe> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
