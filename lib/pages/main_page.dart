bb790b063ff88ea31d171f2bb2d8d3a677285a4a
yymin1022 <yymin1022@gmail.com>
2025-02-08 17:06:04 +0900

    Update meter_page : Seperated page directory

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:taximeter/pages/meter_page.dart';
+import 'package:taximeter/pages/meter_page/meter_page.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
